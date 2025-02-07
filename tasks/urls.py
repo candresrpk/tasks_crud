2d8a7ae40078048364874d7ad841e9c52d99afb7
Carlos Reyes <andrespk3000@gmail.com>
2025-02-06 23:53:37 -0500

    user creation endpoint working

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'tasks'
+
 urlpatterns = [
     path('', views.HomeView, name='index'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
 
-def signupView(rquest):
+def signupView(request):
     
     
     context = {
         'form': UserCreationForm
     }
     
-    return render(rquest, 'users/signup.html', context)
+    if request.method == 'GET':
+        
+        return render(request, 'users/signup.html', context)    
+        
+    else:
+        
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'], 
+                    password=request.POST['password1']
+                )
+                
+                user.save()
+                
+                return redirect('tasks:index')
+            except Exception as e:
+                context['message'] = str(e)
+                return render(request, 'users/signup.html', context)
+            
+        context['message'] = 'Error password does not match'
+        return render(request, 'users/signup.html', context)
