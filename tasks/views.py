9c753f35c6375455a697409f16629297df624200
Carlos Reyes <andrespk3000@gmail.com>
2025-02-11 23:34:27 -0500

    create tasks working, update an delete --pending

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 
@@ -23,8 +23,24 @@ def HomeView(request):
 def createTask(request):
 
     context = {
-
         'form': TaskForm
     }
 
-    return render(request, 'tasks/create.html', context)
+    if request.method == 'GET':
+        return render(request, 'tasks/create.html', context)
+
+    else:
+        try:
+            form = TaskForm(request.POST)
+            if form.is_valid():
+                new_task = form.save(commit=False)
+                new_task.owner = request.user
+                new_task.save()
+            else:
+                context['form'] = form
+                return render(request, 'tasks/create.html', context)
+        except Exception as e:
+            context['message'] = e
+            return render(request, 'tasks/create.html', context)
+
+        return redirect('tasks:home')
